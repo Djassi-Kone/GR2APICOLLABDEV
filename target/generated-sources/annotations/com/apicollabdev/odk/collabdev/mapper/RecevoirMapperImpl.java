5f37b52dd1391a415a49e8bfe89364ada70c1f6c
Professeur7 <professeurhamidoudjire@gmail.com>
2025-08-12 00:53:12 +0200

    Derniere changement

--- target/generated-sources/annotations/com/apicollabdev/odk/collabdev/mapper/RecevoirMapperImpl.java
+++ target/generated-sources/annotations/com/apicollabdev/odk/collabdev/mapper/RecevoirMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-08T10:31:29+0200",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
+    date = "2025-08-11T17:15:32+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
 public class RecevoirMapperImpl implements RecevoirMapper {
