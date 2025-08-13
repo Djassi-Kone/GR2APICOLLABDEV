95847b67d6c461ad1c0d992138e4695b1e222dcc
Deku5005 <dolooumar60@gmail.com>
2025-08-13 10:59:15 +0000

    configuration cross Origin

--- src/main/java/com/apicollabdev/odk/collabdev/CollabdevApplication.java
+++ src/main/java/com/apicollabdev/odk/collabdev/CollabdevApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,5 +18,15 @@ public class CollabdevApplication {
     public static void main(String[] args) {
         SpringApplication.run(CollabdevApplication.class, args);
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
